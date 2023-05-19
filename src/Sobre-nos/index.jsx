d00d3a2c1d489b1dcbb6e2d1488f6b2ac0508703
Rafael <rafael.carbonera@wecode.digital>
2023-05-19 18:45:23 -0300

    Alterações HomeWeCode

---
+++ src/Sobre-nos/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import SobreNosMob from "./Mobile/index";
+
+function SobreNos() {
+  return <SobreNosMob />;
+}
+export default SobreNos;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // import reportWebVitals from './reportWebVitals';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './Home/index.jsx';
-import Header from './Header/index.jsx';
-import Footer from './Footer/index.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home/index.jsx";
+import Header from "./Header/index.jsx";
+import Footer from "./Footer/index.jsx";
+import SobreNos from "./Sobre-nos/index.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Home />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre-nos" element={<SobreNos />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
